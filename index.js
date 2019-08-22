187ed463acfafc8e64499d4bbcd8acb6ae96e8e8
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2019-08-21 22:19:53 -0300

    :sparkles: stock query added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { getBotResponse } from './src/functions/Message';
 import { getInlineResult } from './src/functions/Inline';
+import { getStockData } from './src/functions/Stock';
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
 bot.onText(/^\/q (.*)/i, (msg, match) => getBotResponse(bot, msg, match));
+bot.onText(/^\/s (.*)/i, (msg, match) => getStockData(bot, msg, match));
+
 bot.on('inline_query', data => getInlineResult(bot, data));
