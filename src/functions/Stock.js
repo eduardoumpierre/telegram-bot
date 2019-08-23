3d961ee8c4c2d946ea6562660f8eff2773e3d07e
Schifer <oneiros241@gmail.com>
2019-08-22 23:46:21 -0300

    env.sample. is back

--- src/functions/Stock.js
+++ src/functions/Stock.js
@@ -17,11 +17,11 @@ const getChangeValue = (value, showIcon = false) => {
 const formatResponse = stock => {
   const name = stock['01. symbol'].split('.')[0];
   const price = stock['05. price'];
-  const change = getChangeValue(parseFloat(stock['09. change']).toFixed(2).replace('.',','), true);
+  const change = getChangeValue(parseFloat(stock['09. change']).toFixed(2).replace('.', ','), true);
   const changePercent = getChangeValue(stock['10. change percent']);
 
-  const formatedPrice = parseFloat(price).toFixed(2).replace('.',',');
-  const formatedChangePercent = parseFloat(changePercent).toFixed(2).replace('.',',');
+  const formatedPrice = parseFloat(price).toFixed(2).replace('.', ',');
+  const formatedChangePercent = parseFloat(changePercent).toFixed(2).replace('.', ',');
 
   return {
     response: `${name}\r\n${formatedPrice} BRL\r\n${change} (${formatedChangePercent}%)`,
